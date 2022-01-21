7f938998a3425c99a66c64a0ef6a8f6b1c97e403
CZOLGOWUS <53970486+CZOLGOWUS@users.noreply.github.com>
2022-01-21 11:11:41 +0100

    helper added

--- Assets/_Scripts/Managers/PlayerManager.cs
+++ Assets/_Scripts/Managers/PlayerManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 using CarGame.Units;
+using CarGame.Helpers;
 
 namespace CarGame.Managers
 {
@@ -16,11 +17,12 @@ namespace CarGame.Managers
         private PlayerState _playerCurrentState;
         private PlayerBaseState _playerBaseState = new PlayerBaseState();
 
+        private ViewportArea _gameArea;
 
         [SerializeField] private float _maxSpeed = 10f;
         [Range(0f,1f)]
         [SerializeField] private float _smothingTime = 0.01f;
-
+        [SerializeField] private LayerMask terrainLayer;
 
         public float ZAxis { get; private set; }
         public float XAxis { get; private set; }
@@ -33,33 +35,7 @@ namespace CarGame.Managers
 
         public float SmothingTime { get => _smothingTime; set => _smothingTime = value; }
 
-        struct CameraRays
-        {
-            public Ray topRight;
-            public Ray topLeft;
-            public Ray bottomLeft;
-            public Ray bottomRight;
-        }
 
-        struct CameraRayHits
-        {
-            public RaycastHit topRight;
-            public RaycastHit topLeft;
-            public RaycastHit bottomLeft;
-            public RaycastHit bottomRight;
-        }
-
-        struct CameraBoundriesToWorld
-        {
-            public Vector3 topRight;
-            public Vector3 topLeft;
-            public Vector3 bottomLeft;
-            public Vector3 bottomRight;
-        }
-
-        CameraBoundriesToWorld _gameArea = new CameraBoundriesToWorld();
-        CameraRays _cameraRays = new CameraRays();
-        CameraRayHits cameraRayHits = new CameraRayHits();
 
         private void Awake()
         {
@@ -70,17 +46,7 @@ namespace CarGame.Managers
         // Start is called before the first frame update
         void Start()
         {
-
-            _cameraRays.topLeft = Camera.main.ViewportPointToRay(new Vector3(0, 1, 0));
-            _cameraRays.topRight = Camera.main.ViewportPointToRay(new Vector3(1, 1, 0));
-            _cameraRays.bottomLeft = Camera.main.ViewportPointToRay(new Vector3(0, 0 , 0));
-            _cameraRays.bottomRight = Camera.main.ViewportPointToRay(new Vector3(1, 0, 0));
-
-            Physics.Raycast(_cameraRays.topLeft, out cameraRayHits.topLeft);
-            Physics.Raycast(_cameraRays.topRight, out cameraRayHits.topRight);
-            Physics.Raycast(_cameraRays.bottomLeft, out cameraRayHits.bottomLeft);
-            Physics.Raycast(_cameraRays.bottomRight, out cameraRayHits.bottomRight);
-
+            _gameArea = Helper.GetCameraViewportArea(Camera.main, terrainLayer);
 
             _playerCurrentState = _playerBaseState;
             _playerCurrentState.EnterState(this);
@@ -121,13 +87,6 @@ namespace CarGame.Managers
 
                 _playerCurrentState.Interact(this);
             }
-        }
-
-        private void OnDrawGizmos()
-        {
-            Gizmos.color = Color.red;
-
-            Gizmos.DrawSphere(cameraRayHits.topLeft.point, 0.5f);
         }
 
     }
--- Assets/_Scripts/Utilities/Helper.cs
+++ Assets/_Scripts/Utilities/Helper.cs
@@ -2,10 +2,60 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace CarGame
+namespace CarGame.Helpers
 {
+
     public class Helper
     {
-        
+        public static ViewportArea GetCameraViewportArea(Camera camera, LayerMask layer)
+        {
+            CameraRays _cameraRays = new CameraRays();
+            CameraRayHits _cameraRayHits = new CameraRayHits();
+            ViewportArea _viewportArea = new ViewportArea();
+
+            _cameraRays.topLeft = camera.ViewportPointToRay(new Vector3(0, 1, 0));
+            _cameraRays.topRight = camera.ViewportPointToRay(new Vector3(1, 1, 0));
+            _cameraRays.bottomLeft = camera.ViewportPointToRay(new Vector3(0, 0, 0));
+            _cameraRays.bottomRight = camera.ViewportPointToRay(new Vector3(1, 0, 0));
+
+            Physics.Raycast(_cameraRays.topLeft, out _cameraRayHits.topLeft);
+            Physics.Raycast(_cameraRays.topRight, out _cameraRayHits.topRight);
+            Physics.Raycast(_cameraRays.bottomLeft, out _cameraRayHits.bottomLeft);
+            Physics.Raycast(_cameraRays.bottomRight, out _cameraRayHits.bottomRight);
+
+            _viewportArea.topLeft = _cameraRayHits.topLeft.point;
+            _viewportArea.topRight = _cameraRayHits.topRight.point;
+            _viewportArea.bottomLeft = _cameraRayHits.bottomLeft.point;
+            _viewportArea.bottomRight = _cameraRayHits.bottomRight.point;
+
+            return _viewportArea;
+
+        }
+
+
+    }
+
+    public struct CameraRays
+    {
+        public Ray topRight;
+        public Ray topLeft;
+        public Ray bottomLeft;
+        public Ray bottomRight;
+    }
+
+    public struct CameraRayHits
+    {
+        public RaycastHit topRight;
+        public RaycastHit topLeft;
+        public RaycastHit bottomLeft;
+        public RaycastHit bottomRight;
+    }
+
+    public struct ViewportArea
+    {
+        public Vector3 topRight;
+        public Vector3 topLeft;
+        public Vector3 bottomLeft;
+        public Vector3 bottomRight;
     }
 }
